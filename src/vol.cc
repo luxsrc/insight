31cefe9715c98da6e02cf4a43fc530652671ae09
Chi-kwan Chan <chanc@email.arizona.edu>
2015-03-04 10:16:22 -0500

    mkvol() now reads a binary file to get its data

--- src/vol.cc
+++ src/vol.cc
@@ -23,6 +23,14 @@
 
 unsigned mkvol(const char *name)
 {
+	FILE *file = fopen(name, "rb");
+	if(!file)
+		return 0;
+
+        uint8_t *data = (uint8_t *)malloc(128 * 128 * 128 * 4);
+        fread(data, 1, 128 * 128 * 128 * 4, file);
+        fclose(file);
+
 	unsigned vol;
 	glGenTextures(1, &vol);
 	glBindTexture(GL_TEXTURE_3D, vol);
@@ -32,25 +40,9 @@ unsigned mkvol(const char *name)
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_BORDER);
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-        uint8_t *data = (uint8_t *)malloc(128 * 128 * 128 * 4);
-        for(int i = 0; i < 128; ++i)
-	        for(int j = 0; j < 128; ++j)
-		        for(int k = 0; k < 128; ++k) {
-			        int   h = ((i * 128 + j) * 128 + k) * 4;
-			        float g = exp(-((i-63.5) * (i-63.5) / 4096 +
-			                        (j-63.5) * (j-63.5) / 2048 +
-			                        (k-63.5) * (k-63.5) / 1024));
-			        data[h+0] = 255 * g;
-			        data[h+1] = 255 * g;
-			        data[h+2] = 255 * g;
-			        data[h+3] =  63 * g;
-		        }
-
         glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA,
                      128, 128, 128, 0,
                      GL_RGBA, GL_UNSIGNED_BYTE, data);
-
         free(data);
 
         return vol;
